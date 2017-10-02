b0b45a18f9ba0e70551d0a4852db58214fa1d8e2
Michael Saybolt <sayboltm@egr.msu.edu>
2017-10-02 10:40:22 -0400

    Converted copied leet lib to work as plottr

---
+++ plottr/__init__.py
@@ -0,0 +1,7 @@
+from .text import joke2
+from .plottr import plot, ImShow
+
+def joke():
+    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
+            u'Beiherhund das Oder die Flipperwaldt gersput.')
+
